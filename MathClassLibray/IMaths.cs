f1f338502b378ab434ee363b6d9979ba7173edbd
Saurabh <Saurabh@gamil.com>
2020-03-11 09:36:17 +0530

    Initial commit

---
+++ MathClassLibray/IMaths.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClassLibray
+{
+
+
+        interface IMaths
+        {
+            int Add(int a, int b);
+            int Mul(int a, int b);
+            int Sub(int a, int b);
+            float Divide(int a, int b);
+        }
+    
+}
---
+++ MathClassLibray/Maths.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClassLibray
+{
+    class Maths : IMaths
+    {
+        public int Add(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float Divide(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Mul(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Sub(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
